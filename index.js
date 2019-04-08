042da789051a31d5668a800aa5763bbd4c687ac5
gabeoliver-bicode <37226472+gabeoliver-bicode@users.noreply.github.com>
2019-04-07 21:15:33 -0400

    comments out unused const

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
-const whmcs = new WHMCS({
-  username: '',
-  password: '',
-  apiKey: '',
-  serverUrl: ''
-});
+// const whmcs = new WHMCS({
+//  username: '',
+//  password: '',
+//  apiKey: '',
+//  serverUrl: ''
+// });
 const client = new Discord.Client();
 const settings = require("./config.json")
 var temp = {};
