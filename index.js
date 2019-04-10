8c05a9cd32a79085667a57c60e114e9d12558116
HexDev <hex@hexdev.xyz>
2019-04-10 13:10:35 +0100

    Removed unnecessary duplicates

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ client.on('message', async msg => {
     var verifynum = verifynuminte.toString();
     const embeded = new Discord.RichEmbed()
       .setAuthor(msg.author.username, msg.author.avatarURL)
-      .addField('Verification', 'In order to verify that you are a customer, please send a ticket to the **Verification** Department with the code:\n`' + verifynum + '`\nThen react to this message with a <:okhex:535607625493118986>')
       .addField('Verification', 'In order to verify that you are a customer, please send a ticket to the **Verification** Department with the **The Subject** as:\n`' + verifynum + '`\nThen react to this message with a <:okhex:535607625493118986>')
       .setFooter('This expires in 5 minutes.')
       .setColor('#5b94ef')
@@ -92,7 +91,6 @@ client.on('message', async msg => {
         return;
       }
       tickett = tickets.tickets.ticket
-      var clientid = 40
       var clientid = 0
       var i = 0
       while (clientid == 0 || tickett.length <= i) {
